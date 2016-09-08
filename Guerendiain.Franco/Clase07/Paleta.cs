cd81dc205a62e8afdceb9adca9aa20345e759895
Franco Guerendiain <fguerendiain33@gmail.com>
2016-09-07 22:09:38 -0300

    Clase07

---
+++ Guerendiain.Franco/Clase07/Paleta.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase07
+{
+    class Paleta
+    {
+        #region ATRIBUTOS
+
+        private Tempera[] _colores;
+        private sbyte _cantidadMaximaColores;
+
+        #endregion 
+
+        #region CONSTRUCTORES
+
+        private Paleta(): this(4)
+        {
+        }
+
+        private Paleta(sbyte maxColor)
+        {
+            this._colores = new Tempera[maxColor];
+            this._cantidadMaximaColores = maxColor;
+        }
+
+        #endregion
+
+        #region SOBRECARGA DE OPERADORES
+
+        public static implicit operator Paleta(sbyte cant)
+        {
+            return new Paleta(cant);
+        }
+
+        public static explicit operator string(Paleta p1)
+        {
+            return Mostrar(p1);
+        }
+
+        public static bool operator ==(Paleta p1, Tempera t1)
+        {
+            foreach (Tempera t in p1._colores)
+            {
+                if (t == t1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator !=(Paleta p1, Tempera t1)
+        {
+            return !(p1==t1);
+        }
+
+
+        #endregion
+
+        #region METODOS
+
+        private static string Mostrar(Paleta p1)
+        {
+            string ret = "La cantidad de temperas es " + p1._cantidadMaximaColores + "\n";
+
+            foreach (Tempera t in p1._colores)
+            {
+                ret += t;
+            }
+            return ret;
+        }
+
+        public static Paleta operator +(Paleta p1, Tempera t1)
+        {
+            if (p1 != t1)
+            {
+                int index = nullSearcher(p1);
+
+                if (index >= 0)
+                {
+                    p1._colores[index] = t1;
+                }
+            }
+            return p1;
+        }
+
+        private static int nullSearcher(Paleta p1)
+        {
+            int firstNull = -1;
+            foreach (Tempera t in p1._colores)
+            {
+                if (t.Equals(null))
+                {
+                    firstNull++;
+                }
+            }
+            return firstNull;
+        }
+
+        #endregion
+
+    }
+}
---
+++ Guerendiain.Franco/Clase07/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor[] colores = { ConsoleColor.Red, ConsoleColor.Blue, ConsoleColor.Red };
+            string[] marca = { "sancor", "coca cola", "sancor" };
+            sbyte[] cantidad = {1,2,3};
+
+            Tempera[] temperaArray = new Tempera[3];
+
+            for (int i = 0; i < temperaArray.Length; i++)
+            {
+                temperaArray[i] = new Tempera(colores[i],marca[i],cantidad[i]);
+            }
+
+            foreach (Tempera t in temperaArray)
+            {
+                 Console.WriteLine(t);
+            }
+
+            temperaArray[0] += temperaArray[4];
+
+            Console.WriteLine(temperaArray[0]);
+
+            Paleta p = 3;
+
+            p += temperaArray[0];
+            p += temperaArray[1];
+            p += temperaArray[2];
+
+
+            Console.ReadLine();
+        }
+    }
+}
