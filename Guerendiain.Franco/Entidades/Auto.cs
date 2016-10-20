a420463b927c7897c64a16e03e8215d8f02f8834
Franco Guerendiain <fguerendiain33@gmail.com>
2016-10-19 21:57:08 -0300

    Clase16 - segun maxi

---
+++ Guerendiain.Franco/Entidades/Auto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Auto
+    {
+        #region ATRIBUTOS
+        private string _color;
+        private string _marca;
+        #endregion
+
+        #region PROPIEDADES
+        public string Color
+        {
+            get { return _color; }
+        }
+
+        public string Marca
+        {
+            get { return _marca; }
+        }
+        #endregion
+
+        #region CONSTRUCTORES
+        public Auto(string color, string marca)
+        {
+            this._color = color;
+            this._marca = marca;
+        }
+        #endregion
+
+        #region SOBRECARGA DE OPERADORES
+        public static bool operator ==(Auto a, Auto b)
+        {
+            if(a.Marca == b.Marca && a.Color == b.Color)
+                return true;
+            return false;
+        }
+
+        public static bool operator !=(Auto a, Auto b)
+        {
+            return !(a==b);
+        }
+
+        public override bool Equals(Object obj)
+        {
+            return obj is Auto && ((Auto)obj)==this;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder cadena= new StringBuilder();
+            cadena.Append("Marca: " + this._marca + " - Color: " + this._color);
+
+            return cadena.ToString();
+        }
+        #endregion
+    }
+}
---
+++ Guerendiain.Franco/Entidades/DepositoDeAutos.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class DepositoDeAutos
+    {
+        #region ATRIBUTOS
+        private int _capacidadMaxima;
+        private List<Auto> _lista;
+        #endregion
+
+        #region CONSTRUCTORES
+        private DepositoDeAutos()
+        { 
+            this._lista = new List<Auto>();
+        }
+
+        public DepositoDeAutos(int capacidad): this()
+        {
+            this._capacidadMaxima = capacidad;
+        }
+        #endregion
+
+        #region METODOS
+        public bool Agregar(Auto a)
+        {
+            if (this + a)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Remover(Auto a)
+        {
+            if (this - a)
+            {
+                return true;
+            }
+            return false;
+            
+        }
+
+        private int GetIndice(Auto a)
+        {
+            foreach (Auto t in this._lista)
+            {
+                if (t == a)
+                {
+                    return this._lista.IndexOf(t);
+                }
+            }
+            return -1;
+        }
+
+        #endregion
+
+        #region SOBRECARGA DE OPERADORES
+        public static bool operator +(DepositoDeAutos d, Auto a)
+        {
+            if (d._capacidadMaxima > d._lista.Count)
+            {
+                d._lista.Add(a);
+                return true;
+            }
+            return false;          
+        }
+
+        public static bool operator -(DepositoDeAutos d, Auto a)
+        {
+            if (d.GetIndice(a) != -1)
+            {
+                d._lista.Remove(a);
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder cadena = new StringBuilder();
+            cadena.AppendLine("Capacidad máxima: " + this._capacidadMaxima);
+            foreach (Auto t in this._lista)
+            {
+                cadena.AppendLine(t.ToString());
+            }
+            return cadena.ToString();
+        }
+        #endregion
+
+    }
+}
