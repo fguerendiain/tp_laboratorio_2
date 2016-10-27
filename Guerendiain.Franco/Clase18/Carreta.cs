54d7a432223c6f3c895ad11dacdc1e3994cad31c
Franco Guerendiain <fguerendiain@gmail.com>
2016-10-26 22:03:22 -0300

    Clase18

---
+++ Guerendiain.Franco/Clase18/Carreta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Carreta:Vehiculo, IARBA
+    {
+        public Carreta(double precio): base(precio)
+        { }
+
+        public override void MostrarPrecio()
+        {
+            Console.WriteLine(this._precio);
+        }
+
+        public double CalcularImpuesto()
+        {
+            return this._precio * 1.18;
+        }
+    }
+}
