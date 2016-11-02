841565f9bc5a359333ac10564db84620a3a78c70
unknown <fguerendiain33@gmail.com>
2016-11-02 20:51:10 -0300

    Clase19

---
+++ Guerendiain.Franco/Clase19/Persona.Serializacion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Clase19
+{
+    [XmlInclude(typeof(PersonaH))]
+    public class Persona
+    {
+        public string _nombre;
+        public string _apellido;
+        public long _dni;
+
+
+
+        public Persona()
+        { 
+            
+        }
+
+        public Persona(string nombre, string apellido, long dni)
+        {
+            this._nombre = nombre;
+            this._apellido = apellido;
+            this._dni = dni;
+        }
+
+        public static void MostrarPersona(Persona p)
+        {
+            Console.WriteLine(p.ToString());
+            Console.WriteLine();
+        }
+		
+        public override string ToString()
+        {
+			StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Nombre: "+ this._nombre);
+            sb.AppendLine("Apellido: "+ this._apellido);
+            sb.AppendLine("Dni: "+ this._dni.ToString());
+            sb.AppendLine();
+			return sb.ToString();
+        }
+
+    }
+}
