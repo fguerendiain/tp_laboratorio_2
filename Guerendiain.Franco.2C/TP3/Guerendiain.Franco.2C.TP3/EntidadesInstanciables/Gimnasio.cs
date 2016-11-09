a301ff2781626d5b0a444891c617ace8d49ddc9e
Franco Guerendiain <fguerendiain33@gmail.com>
2016-11-09 19:38:13 -0300

    TP3 algo mas

--- Guerendiain.Franco.2C/TP3/Guerendiain.Franco.2C.TP3/EntidadesInstanciables/Gimnasio.cs
+++ Guerendiain.Franco.2C/TP3/Guerendiain.Franco.2C.TP3/EntidadesInstanciables/Gimnasio.cs
@@ -9,24 +9,59 @@ namespace EntidadesInstanciables
     class Gimnasio
     {
         #region ---------------ATRIBBUTOS--------------
+        private List<Alumno> _alumnos;
+        private List<Instructor> _instructor;
+        private List<Jornada> _jornada;
         #endregion
 
         #region ---------------PROPIEDADES-------------
+        public Jornada this[int i]
+        {
+            get { return this._jornada[i];}
+        }
         #endregion
 
         #region --------------CONSTRUCTORES------------
+        public Gimnasio()
+        {
+            _alumnos = new List<Alumno>();
+            _instructor = new List<Instructor>();
+            _jornada = new List<Jornada>();
+        }
+        
         #endregion
 
         #region -----------------METODOS---------------
+        public static bool Guardar()
+        {
+            return true;
+        }
+
+        public static bool Leer()
+        {
+            return true;
+        }
+
         #endregion
 
         #region ----------SOBRECARGA DE METODOS--------
+        private static string MostrarDatos(Gimnasio gim)
+        {
+            StringBuilder cadena = new StringBuilder();
+            
+            
+            return "";
+        }
+
+        public override string ToString()
+        {
+            return Gimnasio.MostrarDatos(this);
+        }
         #endregion
 
         #region ---------SOBRECARGA DE OPERADORES------
+
         #endregion
 
-        #region ----------------ENUMERADOS-------------
-        #endregion
     }
 }
