f742da105ca966d4a2fd3e892a302e1966b40257
Franco Guerendiain <fguerendiain33@gmail.com>
2016-11-17 01:04:20 -0300

    TP3 Recuperatorio y TP4 Listos para entregar

---
+++ Guerendiain.Franco.2C/RecuperatoriosTP/TP3/Guerendiain.Franco.2C.TP3/EntidadesAbstractas/PersonaGimnasia.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesAbstractas
+{
+    abstract class PersonaGimnasia : Persona
+    {
+        #region ATRIBBUTOS
+        protected int _identificador;
+        #endregion
+
+        #region CONSTRUCTORES
+        public PersonaGimnasia(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+        : base(nombre, apellido, dni, nacionalidad)
+        {
+            this._identificador = id;
+        }
+
+        #endregion
+
+        #region METODOS
+        public virtual string MostrarDatos();
+        public virtual string ParticiparEnClase();
+
+        #endregion
+
+        #region SOBRECARGA DE METODOS
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        #endregion
+
+        #region SOBRECARGA DE OPERADORES
+        public static bool operator ==(PersonaGimnasia pg1, PersonaGimnasia pg2)
+        {
+            return true;
+        }
+
+        public static bool operator !=(PersonaGimnasia pg1, PersonaGimnasia pg2)
+        {
+            return !(pg1==pg2);
+        }
+        #endregion
+
+    }
+}
---
+++ Guerendiain.Franco.2C/TP3/Guerendiain.Franco.2C.TP3/TestUnitarioTP3/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EntidadesAbstractas;
+using EntidadesInstanciables;
+using Excepciones;
+
+namespace TestUnitarioTP3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        
+        public void AtributosNoNulos()
+        {
+            Alumno a1 = new Alumno();
+
+            Assert.IsNotNull(a1._claseQueToma);
+            Assert.IsNotNull(a1._estadoCuenta);
+
+        }
+
+        public void DniNegativo()
+        {
+            try
+            {
+                string menosDni = "-29324565";
+
+                Instructor i1 = new Instructor(1, "Carlos", "Lopez", menosDni, Persona.ENacionalidad.Argentino);
+            }
+            catch
+            {
+                throw new DniInvalidoException("El dni no puede ser Negativo");
+            }
+        }
+
+    }
+}
