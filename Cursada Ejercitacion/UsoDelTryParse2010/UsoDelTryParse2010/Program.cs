0eb64ba0d4c35bcc916fe8ede0858cd9df14b5a0
Franco Guerendiain <fguerendiain@gmail.com>
2017-07-05 10:30:49 -0300

    Reordenamiento de todo el material de estudio, practicas, ejercicios, teoria, parciales

---
+++ Cursada Ejercitacion/UsoDelTryParse2010/UsoDelTryParse2010/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool estaOK;
+            int numero;
+
+            do {
+                //PIDO EL INGRESO DE UN NÚMERO
+                Console.WriteLine("Ingrese un número ");
+
+                //SI NO COINCIDE EL TIPO EL MÉTODO TRYPARSE
+                //RETORNA FALSE, CASO CONTRARIO RETORNA TRUE Y
+                //COLOCA EL VALOR INGRESADO EN LA VARIABLE DE SALIDA
+                estaOK = int.TryParse(Console.ReadLine(), out numero);
+
+                if (!estaOK)
+                    Console.WriteLine("Error en el tipo de dato.");
+
+            }while(!estaOK );
+
+            Console.WriteLine("El número ingresado fue: {0}", numero);
+            Console.ReadLine();
+        }
+    }
+}
